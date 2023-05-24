6337cd0307664fd0ef1dfeb506008318849f2db5
Aakash <aakashsky0202@gmail.com>
2023-05-24 10:17:38 +0530

    Add category API added

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3002;
+
+var cors = require('cors')
+
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 
 app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Aakash12@', 
-  database: 'ram-shiv-db'
+  host: "localhost",
+  user: "root",
+  password: "Aakash12@",
+  database: "ram-shiv-db",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error("Error connecting to the database:", err);
   } else {
-    console.log('Connected to the database');
+    console.log("Connected to the database");
   }
 });
 
-
 // Retrieve all products
-app.get('/products', (req, res) => {
-  connection.query('SELECT * FROM Products', (err, results) => {
+app.get("/products", (req, res) => {
+  connection.query("SELECT * FROM Products", (err, results) => {
     if (err) {
       res.status(500).send(err.message);
     } else {
@@ -39,8 +42,8 @@ app.get('/products', (req, res) => {
 });
 
 // Retrieve all raw materials
-app.get('/rawmaterials', (req, res) => {
-  connection.query('SELECT * FROM RawMaterials', (err, results) => {
+app.get("/rawmaterials", (req, res) => {
+  connection.query("SELECT * FROM RawMaterials", (err, results) => {
     if (err) {
       res.status(500).send(err.message);
     } else {
@@ -50,10 +53,10 @@ app.get('/rawmaterials', (req, res) => {
 });
 
 // Create a new outlet
-app.post('/outlets', (req, res) => {
+app.post("/outlets", (req, res) => {
   const { outlet_name, address, phone, email } = req.body;
   connection.query(
-    'INSERT INTO Outlets (outlet_name, address, phone, email) VALUES (?, ?, ?, ?)',
+    "INSERT INTO Outlets (outlet_name, address, phone, email) VALUES (?, ?, ?, ?)",
     [outlet_name, address, phone, email],
     (err, result) => {
       if (err) {
@@ -65,11 +68,26 @@ app.post('/outlets', (req, res) => {
   );
 });
 
+app.post("/categories", (req, res) => {
+  const { categoryName } = req.body;
+  connection.query(
+    "INSERT INTO categories (name) VALUES (?)",
+    [categoryName],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+});
+
 // Create a new product
-app.post('/products', (req, res) => {
+app.post("/products", (req, res) => {
   const { product_name, description, price } = req.body;
   connection.query(
-    'INSERT INTO Products (product_name, description, price) VALUES (?, ?, ?)',
+    "INSERT INTO Products (product_name, description, price) VALUES (?, ?, ?)",
     [product_name, description, price],
     (err, result) => {
       if (err) {
@@ -82,10 +100,10 @@ app.post('/products', (req, res) => {
 });
 
 // Create a new raw material
-app.post('/rawmaterials', (req, res) => {
+app.post("/rawmaterials", (req, res) => {
   const { rawmaterial_name, description } = req.body;
   connection.query(
-    'INSERT INTO RawMaterials (rawmaterial_name, description) VALUES (?, ?)',
+    "INSERT INTO RawMaterials (rawmaterial_name, description) VALUES (?, ?)",
     [rawmaterial_name, description],
     (err, result) => {
       if (err) {
@@ -98,11 +116,11 @@ app.post('/rawmaterials', (req, res) => {
 });
 
 // Update an outlet
-app.put('/outlets/:id', (req, res) => {
+app.put("/outlets/:id", (req, res) => {
   const { outlet_name, address, phone, email } = req.body;
   const outletId = req.params.id;
   connection.query(
-    'UPDATE Outlets SET outlet_name = ?, address = ?, phone = ?, email = ? WHERE outlet_id = ?',
+    "UPDATE Outlets SET outlet_name = ?, address = ?, phone = ?, email = ? WHERE outlet_id = ?",
     [outlet_name, address, phone, email, outletId],
     (err, result) => {
       if (err) {
@@ -110,5 +128,6 @@ app.put('/outlets/:id', (req, res) => {
       } else {
         res.sendStatus(200);
       }
-    });
+    }
+  );
 });
