aefb6ba7f5edd690c3e96f65917925d6eaec67b3
Aakash <aakashsky0202@gmail.com>
2023-05-30 23:28:49 +0530

    Added get and add product API

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3002;
 
-var cors = require('cors')
+var cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
@@ -31,15 +31,76 @@ connection.connect((err) => {
 });
 
 // Retrieve all products
-app.get("/products", (req, res) => {
-  connection.query("SELECT * FROM Products", (err, results) => {
-    if (err) {
-      res.status(500).send(err.message);
+app.get('/products', (req, res) => {
+  const query = `
+    SELECT 
+      p.*, 
+      c.*, 
+      w.warehouse_id, 
+      w.quantity_available AS warehouse_qty, 
+      o.outlet_id, 
+      o.quantity_available AS outlet_qty
+    FROM 
+      products AS p
+      JOIN categories AS c ON p.category_id = c.id
+      LEFT JOIN warehouse_inventory AS w ON p.id = w.product_id
+      LEFT JOIN outlet_inventory AS o ON p.id = o.product_id
+  `;
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      res.status(500).json({ error: error.message });
     } else {
-      res.json(results);
+      const productData = [];
+      const productMap = {};
+
+      for (const row of results) {
+        const {
+          id,
+          product_name,
+          category_id,
+          category_name,
+          warehouse_id,
+          warehouse_qty,
+          outlet_id,
+          outlet_qty,
+        } = row;
+
+        if (!productMap[id]) {
+          productMap[id] = {
+            id,
+            product_name,
+            category: {
+              category_id,
+              category_name,
+            },
+            warehouse_stock: [],
+            outlet_stock: [],
+          };
+
+          productData.push(productMap[id]);
+        }
+
+        if (warehouse_id && warehouse_qty !== null) {
+          productMap[id].warehouse_stock.push({
+            warehouse_id,
+            quantity: warehouse_qty,
+          });
+        }
+
+        if (outlet_id && outlet_qty !== null) {
+          productMap[id].outlet_stock.push({
+            outlet_id,
+            quantity: outlet_qty,
+          });
+        }
+      }
+
+      res.json(productData);
     }
   });
 });
+
 
 // Retrieve all raw materials
 app.get("/rawmaterials", (req, res) => {
@@ -68,6 +129,7 @@ app.post("/outlets", (req, res) => {
   );
 });
 
+//Add new category
 app.post("/categories", (req, res) => {
   const { categoryName } = req.body;
   connection.query(
@@ -127,6 +189,65 @@ app.put("/outlets/:id", (req, res) => {
         res.status(500).send(err.message);
       } else {
         res.sendStatus(200);
+      }
+    }
+  );
+});
+
+//Add new expense
+app.post("/add-expense", (req, res) => {
+  const { amount, description, outlet_id } = req.body;
+  const currentDate = new Date();
+  connection.query(
+    "INSERT INTO expenses (amount, description, outlet_id, expense_date) VALUES (?, ?, ?, ?)",
+    [amount, description, outlet_id, currentDate],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+});
+
+//Get expense
+app.post("/get-expense", (req, res) => {
+  const { outlet_id } = req.body;
+  connection.query(
+    "SELECT * from expenses where outlet_id = ?",
+    [outlet_id],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+});
+
+//Update stocks
+app.post("./update-stock", (req, res) => {
+  const { warehouseStock, outletStock, productId } = req.body;
+  connection.query(
+    "UPDATE warehouse_inventory SET quantity_available = ? where product_id = ?",
+    [warehouseStock, productId],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        connection.query(
+          "UPDATE outlet_inventory SET quantity_available = ? where product_id = ?",
+          [outletStock, productId],
+          (err, result2) => {
+            if (err) {
+              res.status(500).send(err.message);
+            } else {
+              res.json(result2);
+            }
+          }
+        );
       }
     }
   );
