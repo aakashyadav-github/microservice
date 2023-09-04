f903f82b7ea52fda7cb3062e2e1977ddc73dd0bd
Aakash <aakashsky0202@gmail.com>
2023-09-04 22:17:58 +0530

    APIs Updated

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ app.post("/add-products",  async (req, res) => {
     // Insert new product into the products table
     const productQuery = `
     INSERT INTO Products (product_name, price, unit, category_id, min_stock) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
-    const productValues = [product_name, price, unit, category_id];
+    const productValues = [product_name, price, unit, category_id, min_stock];
     const productResult = await connection.query(productQuery, productValues);
     const productId = productResult.rows[0].id;
 
