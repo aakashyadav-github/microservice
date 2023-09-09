4c17fadacbcc8f3a4fd7f05322bfe0fe9a208fcf
Aakash <aakashsky0202@gmail.com>
2023-09-09 11:41:55 +0530

    Order Add APIs Updated

--- app/controller/orderController.js
+++ app/controller/orderController.js
@@ -1,8 +1,19 @@
 module.exports = (connection) => {
   return {
     getOrders: (req, res) => {
+      connection.query("SELECT * FROM orders;", (err, result) => {
+        if (err) {
+          res.status(500).send(err.message);
+        } else {
+          res.json(result);
+        }
+      });
+    },
+    addOrder: (req, res) => {
+      const { customer_name, mobile_number, total_amount, type } = req.body;
       connection.query(
-        "SELECT * FROM orders;",
+        `INSERT into orders (customer_name, mobile_number,total_amount, type) VALUES ($1, $2, $3, $4) RETURNING *`,
+        [customer_name, mobile_number, total_amount, type],
         (err, result) => {
           if (err) {
             res.status(500).send(err.message);
@@ -12,17 +23,5 @@ module.exports = (connection) => {
         }
       );
     },
-    addOrder: (req, res) => {
-      const {customer_name, mobile_number,total_amount, type} = req.body;
-      `INSERT into orders (customer_name, mobile_number,total_amount, type) VALUES ($1, $2, $3, $4) RETURNING *`,
-      [customer_name, mobile_number,total_amount, type],
-      (err, result) => {
-        if (err) {
-          res.status(500).send(err.message);
-        }else{
-          res.json(result);
-        }
-      } 
-    }
   };
 };
