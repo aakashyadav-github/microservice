a31d92e4ff7339737f568a5c908dff1351cbe0de
Aakash <aakashsky0202@gmail.com>
2023-09-19 22:28:02 +0530

    updated add order API

--- app/controller/orderController.js
+++ app/controller/orderController.js
@@ -2,19 +2,43 @@ module.exports = (connection) => {
   return {
     getOrders: (req, res) => {
       const { type } = req.query;
-      connection.query("SELECT * FROM orders where type=$1;",[type], (err, result) => {
-        if (err) {
-          res.status(500).send(err.message);
-        } else {
-          res.json(result);
+      connection.query(
+        "SELECT * FROM orders where type=$1;",
+        [type],
+        (err, result) => {
+          if (err) {
+            res.status(500).send(err.message);
+          } else {
+            res.json(result);
+          }
         }
-      });
+      );
     },
     addOrder: (req, res) => {
-      const { customer_name, mobile_number, total_amount, type } = req.body;
+      const {
+        customer_name,
+        mobile_number,
+        total_amount,
+        type,
+        customer_address,
+        delivery_date,
+        delivery_time,
+        discount,
+        note,
+      } = req.body;
       connection.query(
-        `INSERT INTO orders (customer_name, mobile_number, total_amount, type, order_date) VALUES ($1, $2, $3, $4, CURRENT_DATE) RETURNING *`,
-        [customer_name, mobile_number, total_amount, type],
+        `INSERT INTO orders (customer_name, mobile_number, total_amount, type, order_date,customer_address,delivery_date,delivery_time,discount,note) VALUES ($1, $2, $3, $4, CURRENT_DATE, $5,$6,$7,$8,$9) RETURNING *`,
+        [
+          customer_name,
+          mobile_number,
+          total_amount,
+          type,
+          customer_address,
+          delivery_date,
+          delivery_time,
+          discount,
+          note,
+        ],
         (err, result) => {
           if (err) {
             res.status(500).send(err.message);
