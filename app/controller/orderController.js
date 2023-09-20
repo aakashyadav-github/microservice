0ae3d5c22ed28e0625612cfde4420e4f537ec4f1
Aakash <aakashsky0202@gmail.com>
2023-09-20 10:07:16 +0530

    Added new order API

--- app/controller/orderController.js
+++ app/controller/orderController.js
@@ -14,6 +14,21 @@ module.exports = (connection) => {
         }
       );
     },
+    getOrdersTotal: (req, res) => {
+      connection.query(
+        `SELECT
+        SUM(total_amount) AS total_amount_all_orders,
+        SUM(CASE WHEN type = 'special' THEN total_amount ELSE 0 END) AS total_amount_special_orders,
+        SUM(CASE WHEN type = 'normal' THEN total_amount ELSE 0 END) AS total_amount_normal_orders FROM orders;`,
+        (err, result) => {
+          if (err) {
+            res.status(500).send(err.message);
+          } else {
+            res.json(result);
+          }
+        }
+      );
+    },
     addOrder: (req, res) => {
       const {
         customer_name,
--- app/routes/orderRoutes.js
+++ app/routes/orderRoutes.js
@@ -5,6 +5,7 @@ module.exports = (connection) => {
   const orderController = require("../controller/orderController")(connection);
 
   router.get("/get-orders", orderController.getOrders);
+  router.get("/get-orders-total", orderController.getOrdersTotal);
   router.post("/add-order", orderController.addOrder);
   return router;
 };
