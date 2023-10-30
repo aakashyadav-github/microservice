5bfb9f8238927add4ef8dbdaae8121b0048b9b89
Aakash <aakashsky0202@gmail.com>
2023-10-30 22:40:37 +0530

    Employee APIs Created

--- app/controller/employeeController.js
+++ app/controller/employeeController.js
@@ -1,12 +1,12 @@
 module.exports = (connection) => {
   return {
     createEmployee: (req, res) => {
-      const { name, aadhar, mobileno, address, salary, dateofjoining, working_area, working_status, date_of_birthday, fathers_name, mothers_name } =
+      const { name, aadhar, mobileno, address, salary, dateofjoining, working_area, working_status, dob, fathers_name, mothers_name } =
         req.body;
-      const photo = req.file;
+      const photo = req.file ? req.file.path : '';
       connection.query(
-        "INSERT INTO employees (name, aadhar, mobileno, address, salary, dateofjoining, photo, working_area, working_status, date_of_birthday, fathers_name, mothers_name) VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9, $10, $11, $12) RETURNING *",
-        [name, aadhar, mobileno, address, salary, dateofjoining, photo, working_area, working_status, date_of_birthday, fathers_name, mothers_name],
+        "INSERT INTO employees (name, aadhar, mobileno, address, salary, dateofjoining, photo, working_area, working_status, dob, fathers_name, mothers_name) VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9, $10, $11, $12) RETURNING *",
+        [name, aadhar, mobileno, address, salary, dateofjoining, photo, working_area, working_status, dob, fathers_name, mothers_name],
         (err, result) => {
           if (err) {
             res.status(500).send(err.message);
