6652ab03401b1ae571d5dfa09328d65225da4225
Aakash <aakashsky0202@gmail.com>
2024-03-25 00:15:33 +0530

    Updated API for request task

--- app/controller/requestTaskController.js
+++ app/controller/requestTaskController.js
@@ -3,7 +3,7 @@ module.exports = (connection) => {
       createRequestTask: (req, res) => {
         const { requestorid, location, product, quantity, type } = req.body;
         connection.query(
-          `INSERT INTO RequestTasks (requestorid, recieverid, productid, quantity, status, type) VALUES ($1, $2, $3, 'open', $4) RETURNING *;`,
+          `INSERT INTO RequestTasks (requestorid, recieverid, productid, quantity, status, type) VALUES ($1, $2, $3, $4, 'open', $5) RETURNING *;`,
           [requestorid, location, product, quantity, type],
           (err, result) => {
             if (err) {
