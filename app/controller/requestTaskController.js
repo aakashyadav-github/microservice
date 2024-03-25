b8fc02487698385c0c3bbd07163fd13e331a8c2e
Aakash <aakashsky0202@gmail.com>
2024-03-25 22:19:52 +0530

    Added get API for request task

--- app/controller/requestTaskController.js
+++ app/controller/requestTaskController.js
@@ -17,7 +17,7 @@ module.exports = (connection) => {
       getRequestTask: (req, res) => {
         const { id } = req.body;
         connection.query(
-          `SELECT RequestTasks.*, Products.*
+          `SELECT RequestTasks.*, Products.unit, Products.product_name 
           FROM RequestTasks
           JOIN Products ON RequestTasks.productid = Products.id
           WHERE RequestTasks.recieverid = $1;`,
